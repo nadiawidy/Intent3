53f7fe3b6b488dc7d40054cab44a3a6951cb8bc3
Nadia Widy <nadiawidyoktaviani@gmail.com>
2016-10-09 15:16:35 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -19,9 +19,24 @@ public class MainActivity extends AppCompatActivity {
                 dialPhoneNumber("085604330998");
             }
         });
+
+        findViewById(R.id.imageViewSMS).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeSmsMessage("Pesan dari SMK Telkom Malang");
+            }
+        });
     }
 
-    private void dialPhoneNumber(String phoneNumber) {
+    public void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain" );
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+    }
+
+    public void dialPhoneNumber(String phoneNumber) {
         Intent intent = new Intent(Intent.ACTION_DIAL);
         intent.setData(Uri.parse("tel:" + phoneNumber));
         if(intent.resolveActivity(getPackageManager()) != null)
